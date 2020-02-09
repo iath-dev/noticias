56342e8336ceaa4f3b57ea94e9927245ac0c961e
iath-name <developer.iath@gmail.com>
2020-02-08 22:32:29 -0500

    143. Formulario y Modulo CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Header } from './components';
+import { Header, Form } from './components';
 
 function App() {
   return (
     <React.Fragment>
       <Header title="Buscador de Noticias" />
       <div className="container white">
-        <h1>Formulario</h1>
+        <Form />
       </div>
     </React.Fragment>
   );
---
+++ src/components/form/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Form = () => {
+    return ( 
+        <div className={`row ${styles.search}`}>
+            <div className="col s12 m8 offset-m2">
+                <form>
+                    <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
+                    <div className="input-field col s12">
+                        <input
+                            type="submit"
+                            className={`btn-large amber darken-2 ${styles.btn_block}`}
+                            value="buscar"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+     );
+}
+ 
+export default Form;
