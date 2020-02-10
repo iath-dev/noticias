9e71a4e4bcef5b8234da799fee58bdf9e2d0e4a6
iath-dev <developer.iath@gmail.com>
2020-02-10 08:17:03 -0500

    147. Consultando la API y leyendo resultados

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { Header, Form } from './components';
 function App() {
 
   const [category, setCategory] = React.useState('');
+  const [artiles, setArticles] = React.useState([]);
+
+  React.useEffect(() => {
+    const consultApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=co&category=${category}&apiKey=f788063e946a42ed9b5526ac1283f0c6`;
+      const res = await fetch(url);
+      const not = await res.json();
+      setArticles(not.articles);
+    }
+    consultApi();
+  }, [category])
 
   return (
     <React.Fragment>
