80abb1cf38265834ff44b180dcea88e68ca42166
iath-dev <developer.iath@gmail.com>
2020-02-10 07:53:31 -0500

    146. Colocando la Categoria en el State

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Header, Form } from './components';
 
 function App() {
+
+  const [category, setCategory] = React.useState('');
+
   return (
     <React.Fragment>
       <Header title="Buscador de Noticias" />
       <div className="container white">
-        <Form />
+        <Form setCategory={setCategory} />
       </div>
     </React.Fragment>
   );
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -3,14 +3,19 @@ import styles from './index.module.css';
 import { useSelect } from '../../hooks';
 import { Options } from '../../data';
 
-const Form = () => {
+const Form = ({ setCategory }) => {
 
     const [category, CategorySelect] = useSelect('', Options.Categories);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setCategory(category);
+    }
 
     return ( 
         <div className={`row ${styles.search}`}>
             <div className="col s12 m8 offset-m2">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
 
                     <CategorySelect />
